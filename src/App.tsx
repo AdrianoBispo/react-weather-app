a442d362051c7d0f44c29f1104a53ca1bbb04537
Adriano Bispo <drianvini10@gmail.com>
2025-06-07 23:32:00 -0300

    :wrench: chore: App.tsx atualizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { AnimatePresence, motion } from "framer-motion";
+import { useAuth } from "@/hooks/useAuth";
+import { AuthPage } from "@/pages/AuthPage";
+import { DashboardPage } from "@/pages/DashboardPage";
+import { Loader } from "@/components/layout/Loader";
 
 export function App() {
-  const [count, setCount] = useState(0);
+  const { user, isAuthReady } = useAuth();
+
+  if (!isAuthReady) {
+    return <Loader />;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={user ? "dashboard" : "auth"}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        {user ? <DashboardPage /> : <AuthPage />}
+      </motion.div>
+    </AnimatePresence>
   );
 }
